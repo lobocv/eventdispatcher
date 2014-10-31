c13f7f54af5a9ac89b3e128859bd63de2508bb7c
clobo <clobo@sensoft.ca>
2014-10-31 13:46:41 -0400

    replaced index, value and key properties with current dictprop.

--- selector.py
+++ selector.py
@@ -1,19 +1,20 @@
 __author__ = 'calvin'
 
 from . import EventDispatcher
-from properties import Property
+from properties import Property, DictProperty
 
 
 class Selector(EventDispatcher):
-    index = Property(0)
-    value = Property(None)
-    key = Property(None)
+    current = DictProperty({})
 
     def __init__(self, options=[], keys=[], wrap=True, **kwargs):
         super(Selector, self).__init__(**kwargs)
         self.wrap = wrap
         self.keys = []
         self.options = []
+        self.register_event('value')
+        self.register_event('index')
+        self.register_event('key')
         self.set_options(options, keys)
 
     def set_options(self, options, keys=[]):
@@ -23,17 +24,39 @@ class Selector(EventDispatcher):
         else:
             self.keys = [str(opt) for opt in options]
         if options:
-            self.dispatch('index', self, self.index)
+            self.current.update({'index': 0, 'value': self.options[0], 'key': self.keys[0]})
+            # self.dispatch_event('index', self, self.index)
 
-    def on_index(self, inst, index):
-        self.key = self.keys[index]
-        self.value = self.options[index]
+    @property
+    def index(self):
+        return self.current['index']
 
-    def on_key(self, inst, key):
-        self.index = self.keys.index(key)
+    @index.setter
+    def index(self, i):
+        self.current.update({'index': i, 'value': self.options[i], 'key': self.keys[i]})
 
-    def on_value(self, inst, value):
-        self.index = self.options.index(value)
+    @property
+    def value(self):
+        return self.current['value']
+
+    @value.setter
+    def value(self, x):
+        i = self.options.index(x)
+        self.current.update({'index': i, 'value': x, 'key': self.keys[i]})
+
+    @property
+    def key(self):
+        return self.current['key']
+
+    @key.setter
+    def key(self, x):
+        i = self.keys.index(x)
+        self.current.update({'index': i, 'value': self.options[i], 'key': x})
+
+    def on_current(self, inst, current):
+        self.dispatch_event('value', inst, current['value'])
+        self.dispatch_event('index', inst, current['index'])
+        self.dispatch_event('key', inst, current['key'])
 
     def next(self, *args):
         index = self.index + 1
