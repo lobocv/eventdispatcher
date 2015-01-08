ae4073951cf379e96f96744850a2335eb31d95f0
clobo <clobo@sensoft.ca>
2015-01-08 13:41:53 -0500

    prepare for pip packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+__author__ = 'lobocv'
+
+from distutils.core import setup
+from eventdispatcher import __version__
+
+setup(
+    name='eventdispatcher',
+    packages=['eventdispatcher'],
+    version=__version__,
+    description='An event dispatcher framework inspired by the Kivy project.',
+    author='Calvin Lobo',
+    author_email='calvinvlobo@gmail.com',
+    url='https://github.com/lobocv/eventdispatcher',
+    download_url='https://github.com/lobocv/eventdispatcher/tarball/%s' % __version__,
+    keywords=['event', 'dispatcher', 'dispatching', 'kivy', 'observer'],
+    classifiers=[],
+)
