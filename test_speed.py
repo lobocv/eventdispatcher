d1530bcc1ba9e114094c22494c536d6d485fe545
clobo <clobo@sensoft.ca>
2015-01-14 10:13:33 -0500

    Added setter speed test and dispatch speed test

--- test_speed.py
+++ test_speed.py
@@ -2,7 +2,7 @@ __author__ = 'Calvin'
 
 from pyperform import BenchmarkedClass, BenchmarkedFunction
 
-from eventdispatcher import EventDispatcher, Property  # !
+from eventdispatcher import EventDispatcher, Property  #!
 
 
 @BenchmarkedClass(classname='Dispatcher')
@@ -13,10 +13,21 @@ class Dispatcher(EventDispatcher):
         super(Dispatcher, self).__init__()
         self.bind(number=self.callback)
 
-    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=10, timeit_number=1000)
-    def run_dispatch(self):
-        for i in xrange(100):
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=10, timeit_number=100)
+    def run_setter(self):
+        for i in xrange(1000):
             self.number = i
+
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=10, timeit_number=100)
+    def run_dispatch(self):
+        for i in xrange(1000):
+            self.dispatch('number', self, i)
+
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=10, timeit_number=100)
+    def run_getter(self):
+        for i in xrange(1000):
+            f = self.number
 
     def callback(self, inst, number):
         pass
+
