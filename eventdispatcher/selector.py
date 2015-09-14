36aacd4b5a37177078dc7b5cbfe0035cbd976b57
clobo <clobo@sensoft.ca>
2015-09-14 09:23:26 -0400

    added __len__ and __copy__ to Selector. remove default key if setting options with a name

--- eventdispatcher/selector.py
+++ eventdispatcher/selector.py
@@ -1,19 +1,19 @@
 __author__ = 'calvin'
 
 from eventdispatcher import EventDispatcher
-from collections import deque
+from collections import deque, OrderedDict
 from itertools import izip_longest
 
 
 class Selector(EventDispatcher):
     # current = DictProperty({})
 
-    def __init__(self, options=[], keys=[], wrap=True, **kwargs):
+    def __init__(self, options=[], keys=[], name='default', wrap=True, **kwargs):
         super(Selector, self).__init__(**kwargs)
         self.wrap = wrap
-        self.current_set = 'default'
+        self.current_set = name
         self.option_sets = {}
-        self.set_options(options, keys)
+        self.set_options(options, keys=keys, name=name)
         self.register_event('value')
         self.register_event('index')
         self.register_event('key')
@@ -21,10 +21,25 @@ class Selector(EventDispatcher):
     def set_options(self, options, keys=[], name='default'):
         options = deque([o for o in izip_longest(options, keys, xrange(len(options)), fillvalue='')])
         self.option_sets[name] = options
-        self.options = options
+        if 'default' in self.option_sets and len(self.option_sets['default']) == 0:
+            del self.option_sets['default']
+        if self.current_set == 'default':
+            self.current_set = name
 
     def __iter__(self):
         return iter(self.option_sets[self.current_set])
+
+    def __len__(self):
+        return len(self.option_sets[self.current_set])
+
+    def __copy__(self):
+        s = Selector(wrap=self.wrap)
+        for name, optset in self.option_sets.iteritems():
+            options, keys, index = zip(*sorted(optset, key=lambda x: x[2]))
+            s.set_options(options, keys, name)
+        s.current_set = self.current_set
+        return s
+
     @property
     def index(self):
         return self.option_sets[self.current_set][0][2]
@@ -86,8 +101,12 @@ class Selector(EventDispatcher):
 
     @property
     def json_repr(self):
-        s = {}
-        for name, optset in self.option_sets.iteritems():
+        s = OrderedDict()
+        option_set_names = self.option_sets.keys()
+        option_set_names.remove(self.current_set)
+        option_set_names.insert(0, self.current_set)
+        for name in option_set_names:
+            optset = self.option_sets[name]
             s[name] = [(v.json_repr if hasattr(v, 'json_repr') else v, key, index) for v, key, index in optset]
         return s
 
