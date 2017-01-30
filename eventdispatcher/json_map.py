0dd7e754f6265dc3488d219a0b5a9b172806ebac
clobo <clobo@sensoft.ca>
2017-01-30 11:21:39 -0500

    Non-first instance creation was not updating property values Created a binding that keeps property values and the underlying JSON object in sync

--- eventdispatcher/json_map.py
+++ eventdispatcher/json_map.py
@@ -1,6 +1,6 @@
 from eventdispatcher import DictProperty, ListProperty, Property, StringProperty, EventDispatcher
 import json as JSON
-import types
+from functools import partial
 
 eventdispatcher_map = {dict: DictProperty,        list: ListProperty,     tuple: Property,
                        int: Property,             float: Property,        long: Property,
@@ -17,24 +17,35 @@ class JSON_Map(EventDispatcher):
     _mapped_classes = {}
 
     def __init__(self, json):
-
+        self.raw = json
         # Check if this class has ever been initialized before, if not
         # perform the definition of event dispatcher properties to the class
         cls = self.__class__
         class_name = cls.__name__
-        properties = {}
-        if class_name not in JSON_Map._mapped_classes:
+        properties = JSON_Map._mapped_classes.get(class_name, None)
+        if properties is None:
             properties = JSON_Map.map_attributes(self, json)
             for prop_name, prop in properties.iteritems():
                 if not hasattr(self, prop_name):
                     setattr(cls, prop_name, prop)
-            JSON_Map._mapped_classes[class_name] = self
+            JSON_Map._mapped_classes[class_name] = properties
 
         super(JSON_Map, self).__init__(json)
 
         with self.temp_unbind_all(*self.event_dispatcher_properties.iterkeys()):
             for key in properties.iterkeys():
-                setattr(self, key, json[key])
+                if key in json:
+                    setattr(self, key, json[key])
+        self.bind(**{p: partial(self._update_raw , p) for p in properties})
+
+    def _update_raw(self, property_name, inst, value):
+        """
+        Callback to keep property values in sync with the underlying JSON object.
+        """
+        if property_name in self.raw:
+            self.raw[property_name] = value
+        else:
+            raise AttributeError('Attribute %s is not found in the underlying JSON object.' % property_name)
 
     @staticmethod
     def map_attributes(obj, json):
