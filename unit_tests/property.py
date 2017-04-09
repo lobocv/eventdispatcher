9cf704956dc4ee977dc3648d0e76cb5e3fc1dfc1
Calvin <calvinlobo@gmail.com>
2017-04-09 11:58:47 -0400

    added speed test to unit test script

--- unit_tests/property.py
+++ unit_tests/property.py
@@ -2,9 +2,13 @@ __author__ = 'calvin'
 import unittest
 import random
 
+from pyperform import timer
+
+
 from . import EventDispatcherTest
 from eventdispatcher import EventDispatcher
 from eventdispatcher import Property
+
 
 
 class Dispatcher(EventDispatcher):
@@ -12,7 +16,8 @@ class Dispatcher(EventDispatcher):
     p2 = Property(20)
 
 
-class PropertyTest(EventDispatcherTest):
+
+class PropertyTest(EventDispatcherTest): #!
 
     def __init__(self, *args):
         super(PropertyTest, self).__init__(*args)
@@ -30,6 +35,36 @@ class PropertyTest(EventDispatcherTest):
             return PropertyTest.create_different_value(value)
         else:
             return different_value
+
+
+
+
+class PropertySpeedTest(EventDispatcher):
+    N_dispatches = 10000
+    p = Property(10)
+
+    @timer
+    def run_setter(self):
+        for i in xrange(PropertySpeedTest.N_dispatches):
+            self.p = PropertyTest.create_different_value(self.p)
+
+    @timer
+    def run_dispatch(self):
+        for i in xrange(PropertySpeedTest.N_dispatches):
+            self.dispatch('p', self, i)
+
+    @timer
+    def run_getter(self):
+        for i in xrange(PropertySpeedTest.N_dispatches):
+            f = self.p
+
+
+speedtest = PropertySpeedTest()
+speedtest.run_getter()
+speedtest.run_setter()
+speedtest.run_dispatch()
+
+
 
 if __name__ == '__main__':
     unittest.main()
