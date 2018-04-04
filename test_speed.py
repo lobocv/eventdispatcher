95473bedf99d4e14188a8bd714e4460c606d8ca3
Calvin Lobo <calvinlobo@gmail.com>
2018-04-04 11:10:49 -0400

    updated speed tests to run a setter for more properties

--- test_speed.py
+++ test_speed.py
@@ -2,36 +2,101 @@ from __future__ import print_function
 from builtins import range
 from pyperform import BenchmarkedClass, BenchmarkedFunction
 
-from eventdispatcher import EventDispatcher, Property, ListProperty, DictProperty  #!
-from tests import EventDispatcherTest #!
+from eventdispatcher import EventDispatcher, Property, ListProperty, DictProperty, SetProperty, UnitProperty  #!
+from eventdispatcher import StringProperty, LimitProperty #!
+from tests.test_property import PropertyTest             #!
+from tests.test_dictproperty import DictPropertyTest     #!
+from tests.test_listproperty import ListPropertyTest     #!
+from tests.test_setproperty import SetPropertyTest       #!
+from tests.test_unitproperty import UnitPropertyTest     #!
+from tests.test_stringproperty import StringPropertyTest #!
+from tests.test_limitproperty import LimitPropertyTest   #!
+
 
 from eventdispatcher import __version__
-print(__version__)
+print("EventDispatcher Version: %s" % __version__)
+print("=" * 50)
+
+
+TIMEIT_REPEAT = 1
+TIMEIT_NUMBER = 1
+INNER_LOOP = int(1e2)  #!
+
 
 @BenchmarkedClass(classname='Dispatcher')
 class Dispatcher(EventDispatcher):
-    # p = ListProperty([1,2,3])
-    p = Property(1)
-    # p = DictProperty({1: 'asd'})
+    listp = ListProperty([1, 2, 3])
+    prop = Property(1)
+    dictp = DictProperty({1: 'asd', 2: 'qwe'})
+    setp = SetProperty(set(range(5)))
+    unitp = UnitProperty(1.0, 'm')
+    stringp = StringProperty('test')
+    limitp = LimitProperty(5, min=0, max=10)
 
     def __init__(self):
         super(Dispatcher, self).__init__()
-        self.bind(p=self.callback)
+        self.bind(listp=self.callback, prop=self.callback, dictp=self.callback)
 
-    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=10, timeit_number=100)
-    def run_setter(self):
-        for i in range(100):
-            self.p = EventDispatcherTest.create_different_value(self.p)
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
+    def run_setter_prop(self):
+        prev_value = self.prop
+        for i in range(INNER_LOOP):
+            self.prop = prev_value = PropertyTest.create_different_value(prev_value)
 
-    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=10, timeit_number=100)
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
+    def run_setter_listp(self):
+        prev_value = self.listp
+        for i in range(INNER_LOOP):
+            self.listp = prev_value = ListPropertyTest.create_different_value(prev_value)
+
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
+    def run_setter_dictprop(self):
+        prev_value = self.dictp
+        for i in range(INNER_LOOP):
+            self.dictp = prev_value = DictPropertyTest.create_different_value(prev_value)
+
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
+    def run_setter_setprop(self):
+        prev_value = self.setp
+        for i in range(INNER_LOOP):
+            self.setp = prev_value = SetPropertyTest.create_different_value(prev_value)
+
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
+    def run_setter_stringprop(self):
+        prev_value = self.stringp
+        for i in range(INNER_LOOP):
+            self.stringp = prev_value = StringPropertyTest.create_different_value(prev_value)
+
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
+    def run_setter_limitprop(self):
+        prev_value = self.limitp
+        for i in range(INNER_LOOP):
+            self.limitp = prev_value = LimitPropertyTest.create_different_value(prev_value)
+
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
+    def run_setter_unitprop(self):
+        prev_value = self.unitp
+        for i in range(INNER_LOOP):
+            self.unitp = prev_value = UnitPropertyTest.create_different_value(prev_value)
+
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
     def run_dispatch(self):
-        for i in range(100):
-            self.dispatch('p', self, i)
+        dispatch = self.dispatch
+        for i in range(INNER_LOOP):
+            dispatch('prop', self, self.prop)
+            dispatch('dictp', self, self.dictp)
+            dispatch('listp', self, self.listp)
+            dispatch('setp', self, self.setp)
+            dispatch('stringp', self, self.stringp)
+            dispatch('limitp', self, self.limitp)
+            dispatch('unitp', self, self.unitp)
 
-    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=10, timeit_number=100)
+    @BenchmarkedFunction(classname='Dispatcher', timeit_repeat=TIMEIT_REPEAT, timeit_number=TIMEIT_NUMBER)
     def run_getter(self):
-        for i in range(100):
-            f = self.p
+        for i in range(INNER_LOOP):
+            prop = self.prop
+            listprop = self.listp
+            dictprop = self.dictp
 
     def callback(self, inst, number):
         pass
